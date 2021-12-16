bc88a493cd0b24e4a44300974376e2b71366b62a
Aries <3055637042@qq.com>
2021-12-16 21:18:55 +0800

    Add something in project4

---
+++ quiz/12/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+  class     Animal{  
+   
+        private:  
+   
+        int     weight;  
+   
+        public:  
+   
+    Animal(    int     w =     0    ){weight = w;}  
+   
+        void     print(){cout     <<     weight     <<     endl;}  
+   
+   };  
+   
+   
+   class     Dog:     public     Animal{  
+   
+        public:  
+   
+    Dog(    int     w =     0    ):Animal(w){}  
+   
+        void     print(){ cout     <<         "Dog "    ; Animal::print();}  
+   
+        virtual    void     speak(){ cout     <<         "wangwang"    <<     endl;}  
+   
+   };  
+   
+   
+   int     main()  
+   
+   {  
+   
+    Dog dog(    5    );  
+   
+    Animal * p = &dog;  
+   
+    p->speak();  
+   
+        return         0    ;  
+   
+   }  
